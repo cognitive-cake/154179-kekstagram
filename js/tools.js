e30a64ccb3ec2fdce758fe8941c45577b14abdb2
Nick-ch <nd.chernikov@gmail.com>
2017-08-20 17:18:51 +0300

    Начало задания №9

---
+++ js/tools.js
@@ -0,0 +1,28 @@
+'use strict';
+
+window.tools = (function () {
+  function getRandomNumber(min, max) {
+    return (Math.random() * (max - min) + min);
+  }
+  function getMaxValue(array) {
+    var maxValue = -1;
+    for (var i = 0; i < array.length; i++) {
+      var currValue = array[i];
+      if (currValue > maxValue) {
+        maxValue = currValue;
+      }
+    }
+    return maxValue;
+  }
+  function getRandomValueOfArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  var obj = {
+    getRandomNumber: getRandomNumber,
+    getMaxValue: getMaxValue,
+    getRandomValueOfArray: getRandomValueOfArray
+  };
+
+  return obj;
+})();
