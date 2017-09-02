77e5d50a1254e26160bd9a81a7a169c8500c15a0
Nick-ch <nd.chernikov@gmail.com>
2017-09-02 22:43:59 +0300

    Разделил код на большее кол-во модулей

--- js/picture.js
+++ js/picture.js
@@ -1,1 +1,47 @@
 'use strict';
+
+window.picture = (function () {
+  var template = document.querySelector('#picture-template');
+
+  // Создание единичного элемента с фотографией
+  function createSinglePhotoElement(photoData) {
+    var cloneNode = template.content.cloneNode('true');
+    cloneNode.querySelector('img').setAttribute('src', photoData.url);
+    cloneNode.querySelector('.picture-likes').textContent = photoData.likes;
+    cloneNode.querySelector('.picture-comments').textContent = photoData.comments.length;
+    return cloneNode;
+  }
+  // Создание списка с фотографиями
+  function createListOfPhotos(array) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < array.length; i++) {
+      var object = array[i];
+      var photo = createSinglePhotoElement(object);
+      fragment.appendChild(photo);
+    }
+    return fragment;
+  }
+  // Определение правильного окончания для слова "n комментариев"
+  function getTrueEndingOfWord(commentsAmount) {
+    var lastNumber = commentsAmount.charAt(commentsAmount.length - 1);
+    var commentWord;
+    if (window.tools.isSecondTen(commentsAmount)) {
+      commentWord = ' комментариев';
+    } else {
+      if (+lastNumber === 1) {
+        commentWord = ' комментарий';
+      } else if (lastNumber > 1 && lastNumber < 5) {
+        commentWord = ' комментария';
+      } else {
+        commentWord = ' комментариев';
+      }
+    }
+    return commentWord;
+  }
+
+  // Экспорт
+  return {
+    createListOfPhotos: createListOfPhotos,
+    getTrueEndingOfWord: getTrueEndingOfWord
+  };
+})();
--- js/preview.js
+++ js/preview.js
@@ -1,1 +1,23 @@
 'use strict';
+
+window.preview = (function () {
+  var galleryOverlay = document.querySelector('.gallery-overlay');
+
+  // Вставка фотографии с данными в оверлей
+  function setPhotoToOverlay(element) {
+    var elementImgSource = element.querySelector('img').getAttribute('src');
+    var elementLikesAmount = element.querySelector('.picture-likes').textContent;
+    var elementCommentsAmount = element.querySelector('.picture-comments').textContent;
+    var commentWord = window.picture.getTrueEndingOfWord(elementCommentsAmount);
+
+    galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', elementImgSource);
+    galleryOverlay.querySelector('.likes-count').textContent = elementLikesAmount;
+    galleryOverlay.querySelector('.comments-count').textContent = elementCommentsAmount;
+    galleryOverlay.querySelector('.comments-word').textContent = commentWord;
+  }
+
+  // Экспорт
+  return {
+    setPhotoToOverlay: setPhotoToOverlay
+  };
+})();
